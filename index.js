1ca28e968255b35de5310e7f19bddb2567f97142
Priyanka <priyanka.njoy9@gmail.com>
2018-10-03 22:54:44 +0530

    Solution1

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-
+console.log('***************Question 1***************')
+var q1 = require('./solutions/q1_pyramid_of_stars');
+console.log(q1(6));
--- solutions/q1_pyramid_of_stars.js
+++ solutions/q1_pyramid_of_stars.js
@@ -1,7 +1,24 @@
 /* Write a program to build a `Pyramid of stars` of given height */
 
-const buildPyramid = () => {
-	// Write your code here
+const buildPyramid = (rows) => {
+    
+    var text = '';
+    for(var i=0 ; i<rows ; i++) {
+        //console.log('i = ' + i);
+        for (var k=1; k<=(rows-(i+1)); k++) {
+            //console.log('       k = ' + k);
+            text += ' ';
+        }
+
+        for (var j=0;j<=i;j++) {
+            //console.log('       j = ' + j);
+            text += ' *';
+        }
+
+        text += '  \n';
+    }
+
+    return text;
 };
 
 /* For example,
