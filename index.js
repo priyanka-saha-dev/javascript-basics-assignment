a601d193de336948c9e7b5f836037f356b3de286
Priyanka <priyanka.njoy9@gmail.com>
2018-10-04 00:36:26 +0530

    Solution3

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 // var q1 = require('./solutions/q1_pyramid_of_stars');
 // console.log(q1(6));
 
-console.log('***************Question 2***************')
-const testObj = [{ id: 1, name: 'Ankit', role: 'Developer'},
-{ id: 2, name: 'Pankhuri', role: 'Lead'},
-{ id: 3, name: 'Anubha', role: 'QA'}];;
-var key = 'role';
+// console.log('***************Question 2***************');
+// const testObj = [{ id: 1, name: 'Ankit', role: 'Developer'},
+// { id: 2, name: 'Pankhuri', role: 'Lead'},
+// { id: 3, name: 'Anubha', role: 'QA'}];;
+// var key = 'role';
 
-var q2 = require('./solutions/q2_array_to_object_converter');
-//q2(testObj, key);
-console.log(q2(testObj, key));
+// var q2 = require('./solutions/q2_array_to_object_converter');
+// //q2(testObj, key);
+// console.log(q2(testObj, key));
+
+console.log('***************Question 3***************');
+var q3 = require('./solutions/q3_flatten_n_dimensional_array');
+ var testObj = [1, [2, 3], [[4, 5], [6, 7]], [[[8, 9], 10]]];
+ 
+console.log(q3(testObj));
--- solutions/q3_flatten_n_dimensional_array.js
+++ solutions/q3_flatten_n_dimensional_array.js
@@ -1,8 +1,38 @@
 /* Write a Program to Flatten a given n-dimensional array */
 
-const flatten = () => {
-	// Write your code here
+const flatten = (testObj) => {
+
+	// var res = [];
+
+	// if(Array.isArray(testObj)) {
+	// 	flatArray(testObj, res);
+	// } else {
+	// 	res = null;
+	// }
+
+	// return res;
+
+	if (Array.isArray(testObj)) {
+		return flattenDeep(testObj);
+	} else {
+		return null;
+	}
 };
+
+// function flatArray(arrayObj, res) {
+
+// 	for (var i = 0; i <= arrayObj.length; i++) {
+// 		if (Array.isArray(arrayObj[i])) {
+// 			flatArray(arrayObj[i], res);
+// 		} else if (arrayObj[i] != undefined) {
+// 			res[res.length] = arrayObj[i];
+// 		}
+// 	}
+// }
+
+function flattenDeep(arr1) {
+	return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+}
 
 /* For example,
 INPUT - flatten([1, [2, 3], [[4], [5]])
